cb6dab1af43d994db40b68d6ec7425f780bf3791
aMiNjOn1065 <aminjon1065@gmail.com>
2025-05-28 16:21:33 +0500

    tests

--- resources/js/components/ui/pagination.tsx
+++ resources/js/components/ui/pagination.tsx
@@ -6,7 +6,7 @@ import {
 } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
-import { Button, buttonVariants } from '@/Components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
 import { Link } from '@inertiajs/react';
 import { ComponentProps } from 'react';
 
